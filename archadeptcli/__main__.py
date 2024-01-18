f06371a4724028ca5dd3719788dcecce3ab9fd01
Ash Wilding <ash@archadept.com>
2024-01-18 14:51:30 +0000

    [release] Publish archadeptcli-v1.1.2

--- archadeptcli/__main__.py
+++ archadeptcli/__main__.py
@@ -38,7 +38,7 @@ from archadeptcli.console import getConsole, RichAlign, RichGroup, RichPanel
 from archadeptcli.docker import DockerCLIWrapper
 from archadeptcli.exceptions import *
 
-ARCHADEPTCLI_BASE_TAG = '1.1.1'
+ARCHADEPTCLI_BASE_TAG = '1.1.2'
 """ Current base tag of the archadeptcli repository. """
 
 class CommandLineArgs():
