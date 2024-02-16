ArchAdept/archadeptcli

commit d3a967604f97106a313495221060977b1174938d

Pick up user's color palette rather than using hardcoded colors
Until now we've been styling Rich text using e.g. `style='yellow'` for
warnings and `style='red'` for errors, however these values actually
map onto absolute color codes which can lead to very ugly text depending
on the user's chosen color palette in their terminal app (especially on
Windows PowerShell's default color palette).

We can do better here by using e.g. `style='color(3)'` for warnings and
`style='color(1)'` for errors, which map onto the `OLIVE` and `RED`
XTerm color palette slots respectively and so will be styled according
to the user's own chosen color palette values.

archadeptcli/__main__.py
@@ -34,7 +34,7 @@ rich.traceback.install(show_locals=True, suppress=[rich])
 
 # Local deps
 import archadeptcli
-from archadeptcli.console import getConsole, RichAlign, RichGroup, RichPanel
+from archadeptcli.console import getConsole, RichAlign, RichGroup, RichPanel, Color
 from archadeptcli.docker import DockerCLIWrapper
 from archadeptcli.exceptions import *
 
@@ -358,11 +358,11 @@ def print_qemu_help_message(container_id:str=None) -> None:
     if container_id is not None:
         renderables.append('Simulation is paused waiting for debugger.\n' \
                                'Run this command in another window to attach the debugger:')
-        debug_panel = RichPanel.fit(f'$ archadept debug {container_id}', style='blue')
+        debug_panel = RichPanel.fit(f'$ archadept debug {container_id}', style=Color.EXTRA)
         renderables.append(RichAlign.center(debug_panel))
     renderables.append('Press \'Ctrl-a\' followed by \'x\' to end the simulation.\n' \
                        'QEMU is now controlling this terminal window until the simulation ends...')
-    getConsole().print(RichPanel.fit(RichGroup(*renderables), style='green'))
+    getConsole().print(RichPanel.fit(RichGroup(*renderables), style=Color.INFO))
 
 def main_run(image:str, tag:str, workdir:Path, spawn_gdbserver:bool) -> int:
     """ Main function for ``archadept run``.

archadeptcli/exceptions.py
@@ -26,7 +26,7 @@ DEALINGS IN THE SOFTWARE.
 from typing import Optional
 
 # Local deps
-from .console import getConsole, RichAlign, RichGroup, RichPanel
+from .console import getConsole, RichAlign, RichGroup, RichPanel, Color
 
 
 class ArchAdeptWarning():
@@ -59,10 +59,10 @@ class ArchAdeptWarning():
             renderables += [f'{self.tip}\n']
         renderables += ['See https://archadept.com/help/warnings for more help with this warning.']
         console = getConsole()
-        console.print(RichPanel.fit(RichGroup(*renderables), style='yellow',
+        console.print(RichPanel.fit(RichGroup(*renderables), style=Color.WARNING,
                                     title=f'ArchAdept warning 0x{self.unique_id:04X}'))
         if not console.debug_enabled:
-            console.print(RichPanel.fit(f'Note: You can also rerun with `-v` for more verbose debug logging.', style='yellow'))
+            console.print(RichPanel.fit(f'Note: You can also rerun with `-v` for more verbose debug logging.', style=Color.WARNING))
 
 class ProjectRunSupportUnknown(ArchAdeptWarning):
     """ Flagged when we are unable to determine whether an example project
@@ -114,10 +114,10 @@ class ArchAdeptError(Exception):
         else:
             renderables += ['See https://archadept.com/help/errors for more help with this error.']
         console = getConsole()
-        console.print(RichPanel.fit(RichGroup(*renderables), style='red',
+        console.print(RichPanel.fit(RichGroup(*renderables), Color.ERROR,
                                     title=f'!!! ArchAdept error 0x{self.unique_id:04X} !!!'))
         if not console.debug_enabled:
-            console.print(RichPanel.fit(f'Note: You can also rerun with `-v` for more verbose debug logging.', style='red'))
+            console.print(RichPanel.fit(f'Note: You can also rerun with `-v` for more verbose debug logging.', style=Color.ERROR))
 
 class UngracefulExit(ArchAdeptError):
     """ Raised when we crash due to any kind of uncaught exception. """

archadeptcli/console.py
@@ -30,6 +30,14 @@ from rich.align import Align as RichAlign
 from rich.console import Console as RichConsole, Group as RichGroup
 from rich.panel import Panel as RichPanel
 
+class Color():
+    """ Color code mappings so that we pickup the user's own color palette. """
+    DEBUG   = 'color(7)'  # SILVER
+    INFO    = 'color(2)'  # GREEN
+    WARNING = 'color(3)'  # OLIVE
+    ERROR   = 'color(1)'  # RED
+    EXTRA   = 'color(6)'  # TEAL
+
 class Console():
     """ Wrapper around upstream Rich Console used for all textual output. """
 
@@ -94,5 +102,6 @@ __all__ = [
     'RichConsole',
     'RichGroup',
     'RichPanel',
+    'Color',
 ]
 

archadeptcli/docker.py
@@ -33,7 +33,7 @@ from pathlib import Path
 from typing import Optional
 
 # Local deps
-from .console import getConsole, RichGroup, RichPanel
+from .console import getConsole, RichGroup, RichPanel, Color
 from .exceptions import *
 
 @dataclass
@@ -104,7 +104,7 @@ class DockerCLIWrapper():
         its captured and combined ``stdout`` plus ``stderr``, else ``None``.
         """
         full_command = f'{self.docker_cli_binary} {command}'.rstrip()
-        getConsole().debug(RichPanel.fit(full_command, style='white', title='invoking command...'))
+        getConsole().debug(RichPanel.fit(full_command, style=Color.DEBUG, title='invoking command...'))
         # Spawn the subprocess with appropriate piping.
         output = None
         kwargs = {'text':True, 'encoding':'UTF-8'}
@@ -152,7 +152,7 @@ class DockerCLIWrapper():
                     RichPanel.fit(
                         result.command,
                         title='command',
-                        style='blue',
+                        style=Color.EXTRA,
                     ),
                     RichPanel(
                         result.output,

commit 00cba45f1e4186400d5a47e51da0c8f53acbbd4f

[bug] Fix crash when rendering errors
Commit d3a967604f971 introduced picking up the user's color palette
when rendering Rich text and panels, instead of hardcoding the color.

However, a missing `style=` when rendering errors meant that we were
erroneously passing the value of `Color.RED` -- that is, `'color(1)'`,
through to the Rich panel as a positional arg rather than a kwarg,
which crashes the Rich renderer because raw strings do not implement
the Rich renderable API.

While we're here, ensure we always render errors even when `-v` is
passed on the command line to enable debug logging.

archadeptcli/__main__.py
@@ -465,10 +465,10 @@ def main():
         else:
             raise InternalError(f'unimplemented function: main_{args.command}()')
     except ArchAdeptError as e:
+        e.render()
         if args.debug:
             raise e
         else:
-            e.render()
             return 1
     except Exception as e:
         raise UngracefulExit('crashed due to uncaught exception') from e

archadeptcli/exceptions.py
@@ -118,7 +118,7 @@ class ArchAdeptError(Exception):
         else:
             renderables += ['See https://archadept.com/help/errors for more help with this error.']
         console = getConsole()
-        console.print(RichPanel.fit(RichGroup(*renderables), Color.ERROR,
+        console.print(RichPanel.fit(RichGroup(*renderables), style=Color.ERROR,
                                     title=f'!!! ArchAdept error 0x{self.unique_id:04X} !!!'))
         if not console.debug_enabled:
             console.print(RichPanel.fit(f'Note: You can also rerun with `-v` for more verbose debug logging.', style=Color.ERROR))